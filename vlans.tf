9414601b376eeeb2280306d5f27a94c8ee487453
dchosnek <dchosnek@cisco.com>
2021-05-04 17:56:37 -0500

    updated VLAN IDs

--- vlans.tf
+++ vlans.tf
@@ -22,8 +22,8 @@ resource "intersight_fabric_eth_network_policy" "fabric_eth_network_policy1" {
 resource "intersight_fabric_vlan" "fabric_vlan1" {
   auto_allow_on_uplinks = true
   is_native             = false
-  name                  = "imc_105"
-  vlan_id               = 105
+  name                  = "vlan1010"
+  vlan_id               = 1010
   multicast_policy {
     moid = intersight_fabric_multicast_policy.fabric_multicast_policy1.moid
   }
@@ -31,27 +31,4 @@ resource "intersight_fabric_vlan" "fabric_vlan1" {
     moid = intersight_fabric_eth_network_policy.fabric_eth_network_policy1.id
   }
 }
-resource "intersight_fabric_vlan" "fabric_vlan2" {
-  auto_allow_on_uplinks = true
-  is_native             = false
-  name                  = "os_106"
-  vlan_id               = 106
-  multicast_policy {
-    moid = intersight_fabric_multicast_policy.fabric_multicast_policy1.moid
-  }
-  eth_network_policy {
-    moid = intersight_fabric_eth_network_policy.fabric_eth_network_policy1.id
-  }
-}
-resource "intersight_fabric_vlan" "fabric_vlan3" {
-  auto_allow_on_uplinks = true
-  is_native             = false
-  name                  = "imm_109"
-  vlan_id               = 109
-  multicast_policy {
-    moid = intersight_fabric_multicast_policy.fabric_multicast_policy1.moid
-  }
-  eth_network_policy {
-    moid = intersight_fabric_eth_network_policy.fabric_eth_network_policy1.id
-  }
-}
+
