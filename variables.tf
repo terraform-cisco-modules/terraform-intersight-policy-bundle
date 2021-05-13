b55965f92e8ce12bb9b552ca0ec2d41a8dc872d9
dchosnek <dchosnek@cisco.com>
2021-05-12 21:49:40 -0500

    formatting

--- variables.tf
+++ variables.tf
@@ -22,28 +22,33 @@ variable "tags" {
 }
 
 variable "server_ports_6454" {
-  type = set(string)
+  type        = set(string)
   description = "list of port numbers to be assigned to server ports"
 }
 variable "port_channel_6454" {
-  type = set(string)
+  type        = set(string)
   description = "list of port numbers to be assigned to uplink port channel"
 }
 variable "fc_port_count_6454" {
-  type = number
+  type        = number
   description = "number of ports to assign to FC starting at port 1"
-  default = 0
+  default     = 0
 }
 variable "uplink_vlans_6454" {
-  type = map(number)
+  type        = map(number)
   description = "map of vlan names and IDs to be used on FI uplinks"
 }
 
+variable "vnic_mac_pool" {
+  type        = string
+  description = "moid of MAC pool for use with vnics"
+}
+
 variable "imc_access_pool" {
-  type = string
+  type        = string
   description = "moid of IP pool for IMC access"
 }
 variable "imc_access_vlan" {
-  type = number
+  type        = number
   description = "ID of VLAN for IMC access"
 }
