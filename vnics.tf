ebe0a3828b7112b5f03456cbbfa933b526dc7c74
dchosnek <dchosnek@cisco.com>
2021-05-12 21:46:45 -0500

    added lan connectivity for servers

--- vnics.tf
+++ vnics.tf
@@ -3,7 +3,6 @@
 resource "intersight_fabric_eth_network_group_policy" "fabric_eth_network_group_policy1" {
   name        = "${var.policy_prefix}-eth-network-group"
   description = var.description
-  # target_platform = "FIAttached"
   vlan_settings {
     native_vlan   = 1
     allowed_vlans = join(",", values(var.uplink_vlans_6454))
@@ -71,6 +70,143 @@ resource "intersight_vnic_eth_adapter_policy" "v_eth_adapter1" {
   description = var.description
   organization {
     moid = var.organization
+  }
+  dynamic "tags" {
+    for_each = var.tags
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+}
+
+
+# =============================================================================
+# LAN Connectivity
+# -----------------------------------------------------------------------------
+
+resource "intersight_vnic_lan_connectivity_policy" "vnic_lan1" {
+  name                = "${var.policy_prefix}-lan-connectivity"
+  description         = var.description
+  iqn_allocation_type = "None"
+  placement_mode      = "auto"
+  target_platform     = "FIAttached"
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+  # add policy to the specified server profile template
+  profiles {
+    moid        = intersight_server_profile_template.template1.moid
+    object_type = "server.ProfileTemplate"
+  }
+  dynamic "tags" {
+    for_each = var.tags
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+}
+
+# =============================================================================
+# vNICs
+# -----------------------------------------------------------------------------
+resource "intersight_vnic_eth_if" "eth0" {
+  name             = "eth0"
+  order            = 0
+  failover_enabled = false
+  mac_address_type = "POOL"
+  mac_pool {
+    moid = var.vnic_mac_pool
+  }
+  placement {
+    id        = ""
+    pci_link  = 0
+    switch_id = "A"
+    uplink    = 0
+  }
+  cdn {
+    value     = "eth0"
+    nr_source = "vnic"
+  }
+  usnic_settings {
+    cos      = 5
+    nr_count = 0
+  }
+  vmq_settings {
+    enabled             = false
+    multi_queue_support = false
+    num_interrupts      = 16
+    num_vmqs            = 4
+  }
+  lan_connectivity_policy {
+    moid        = intersight_vnic_lan_connectivity_policy.vnic_lan1.id
+    object_type = "vnic.LanConnectivityPolicy"
+  }
+  eth_adapter_policy {
+    moid = intersight_vnic_eth_adapter_policy.v_eth_adapter1.id
+  }
+  eth_qos_policy {
+    moid = intersight_vnic_eth_qos_policy.v_eth_qos1.id
+  }
+  fabric_eth_network_group_policy {
+    moid = intersight_fabric_eth_network_group_policy.fabric_eth_network_group_policy1.moid
+  }
+  fabric_eth_network_control_policy {
+    moid = intersight_fabric_eth_network_control_policy.fabric_eth_network_control_policy1.moid
+  }
+  dynamic "tags" {
+    for_each = var.tags
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+}
+resource "intersight_vnic_eth_if" "eth1" {
+  name             = "eth1"
+  order            = 0
+  failover_enabled = false
+  mac_address_type = "POOL"
+  mac_pool {
+    moid = var.vnic_mac_pool
+  }
+  placement {
+    id        = ""
+    pci_link  = 0
+    switch_id = "B"
+    uplink    = 0
+  }
+  cdn {
+    value     = "eth1"
+    nr_source = "vnic"
+  }
+  usnic_settings {
+    cos      = 5
+    nr_count = 0
+  }
+  vmq_settings {
+    enabled             = false
+    multi_queue_support = false
+    num_interrupts      = 16
+    num_vmqs            = 4
+  }
+  lan_connectivity_policy {
+    moid        = intersight_vnic_lan_connectivity_policy.vnic_lan1.id
+    object_type = "vnic.LanConnectivityPolicy"
+  }
+  eth_adapter_policy {
+    moid = intersight_vnic_eth_adapter_policy.v_eth_adapter1.id
+  }
+  eth_qos_policy {
+    moid = intersight_vnic_eth_qos_policy.v_eth_qos1.id
+  }
+  fabric_eth_network_group_policy {
+    moid = intersight_fabric_eth_network_group_policy.fabric_eth_network_group_policy1.moid
+  }
+  fabric_eth_network_control_policy {
+    moid = intersight_fabric_eth_network_control_policy.fabric_eth_network_control_policy1.moid
   }
   dynamic "tags" {
     for_each = var.tags
