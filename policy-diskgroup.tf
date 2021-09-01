e00f5c56f3aa7efaec44f2e0725d3ce9609aa885
Brandon Beck <beckbran@gmail.com>
2021-09-01 08:32:56 -0500

    changed to storage drive groups

--- policy-diskgroup.tf
+++ policy-diskgroup.tf
@@ -4,27 +4,29 @@
 
 # create a RAID mirror between the first two physical disks
 
-resource "intersight_storage_disk_group_policy" "group1" {
+resource "intersight_storage_drive_group" "group1" {
   description = var.description
   name        = "${var.policy_prefix}-disk-group"
   raid_level  = "Raid1"
-  span_groups = [{
-    additional_properties = ""
-    class_id              = "storage.SpanGroup"
-    disks = [{
-      additional_properties = ""
-      class_id              = "storage.LocalDisk"
-      object_type           = "storage.LocalDisk"
-      slot_number           = 1
-      },
+  manual_drive_group = [{
+    span_groups = [
       {
-        additional_properties = ""
-        class_id              = "storage.LocalDisk"
-        object_type           = "storage.LocalDisk"
-        slot_number           = 2
+        slots = "1,2"
       }
     ]
-    object_type = "storage.SpanGroup"
+  }]
+  virtual_drives = [{
+    boot_drive = false
+    expand_to_available = true
+    name = "vd0"
+    size = 0
+    virtual_drive_policy = [{
+      access_policy = "Default"
+      drive_cache = "Default"
+      read_policy = "Default"
+      strip_size = 64
+      write_policy = "Default"
+    }]
   }]
   organization {
     moid = var.organization
@@ -43,32 +45,11 @@ resource "intersight_storage_disk_group_policy" "group1" {
 resource "intersight_storage_storage_policy" "storage1" {
   description                  = var.description
   name                         = "${var.policy_prefix}-storage"
-  retain_policy_virtual_drives = false
   unused_disks_state           = "UnconfiguredGood"
-  disk_group_policies = [{
-    additional_properties = ""
+  drive_group = [{
     moid                  = intersight_storage_disk_group_policy.group1.moid
     class_id              = "mo.MoRef"
     object_type           = "storage.DiskGroupPolicy"
-    selector              = ""
-  }]
-  virtual_drives = [{
-    access_policy         = "Default"
-    additional_properties = ""
-    boot_drive            = false
-    class_id              = "storage.VirtualDriveConfig"
-    disk_group_name       = intersight_storage_disk_group_policy.group1.name
-    disk_group_policy     = intersight_storage_disk_group_policy.group1.moid
-    drive_cache           = "Default"
-    expand_to_available   = true
-    io_policy             = "Default"
-    name                  = "vd0"
-    object_type           = "storage.VirtualDriveConfig"
-    read_policy           = "Default"
-    size                  = 0
-    strip_size            = "Default"
-    vdid                  = "value"
-    write_policy          = "Default"
   }]
   organization {
     moid = var.organization
