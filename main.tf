2d7ce316cb5ff3c04bfe473af54cc355f4ef4b87
dchosnek <dchosnek@cisco.com>
2022-04-06 14:22:28 -0500

    changed mappings syntax for vMedia policy

--- main.tf
+++ main.tf
@@ -218,24 +218,22 @@ resource "intersight_vmedia_policy" "vmedia1" {
   enabled       = true
   encryption    = false
   low_power_usb = true
-  mappings = [{
+  mappings {
     additional_properties   = ""
     authentication_protocol = "none"
     class_id                = "vmedia.Mapping"
     device_type             = "cdd"
     file_location           = "infra-chx.auslab.cisco.com/software/linux/ubuntu-18.04.5-server-amd64.iso"
     host_name               = "infra-chx.auslab.cisco.com"
-    is_password_set         = false
     mount_options           = "RO"
     mount_protocol          = "nfs"
     object_type             = "vmedia.Mapping"
     password                = ""
     remote_file             = "ubuntu-18.04.5-server-amd64.iso"
     remote_path             = "/iso/software/linux"
-    sanitized_file_location = "infra-chx.auslab.cisco.com/software/linux/ubuntu-18.04.5-server-amd64.iso"
     username                = ""
     volume_name             = "IMC_DVD"
-  }]
+  }
   organization {
     moid        = var.organization
     object_type = "organization.Organization"
